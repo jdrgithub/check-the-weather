d771b05209387f026243fb32a16e9df9d71cde59
Jonathan Rooke <jdoggus@gmail.com>
2024-09-04 22:41:05 -0400

    Enabled SQL Alchemy template

--- app/flask_minimum.py
+++ app/flask_minimum.py
@@ -6,11 +6,26 @@ This is the core flask template to build on.
 
 from flask import Flask
 from routes import main
+from flask_sqlalchemy import SQLAlchemy
 
-# Create instance of Flask class
-# The __name__ variable used for setting up paths.
+# FLASK INSTANCE
 app = Flask(__name__)
+# CREATE LOCAL DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional: Disable track modifications to suppress warnings
+# SQLALCHEMY INSTANCE
+db = SQLAlchemy(app)
+# REGISTER ROUTES
 app.register_blueprint(main)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}'
 
 
 # Define a route for root URL -> tells Flask what URL triggers `home` function.
